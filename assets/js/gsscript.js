11e6e7aca1e058029244d1341e8b8b66447a349d
Ajay Kumar <devakm95@gmail.com>
2019-10-29 21:09:32 +0530

    Update gsscript.js

--- assets/js/gsscript.js
+++ assets/js/gsscript.js
@@ -22,10 +22,9 @@ const app = function () {
 
 	function _getPosts () {
 		_setNotice('<img class="loader" src="./assets/img/lo.jpg" alt="loading"/>');
-var header = new Headers();
-		header('Access-Control-Allow-Origin', '*')
+
 		fetch(_buildApiUrl(state.activePage, state.activeCategory),{
-    headers: header
+    headers: {'Access-Control-Allow-Origin', '*'}
 })
 			.then((response) => response.json())
 			.then((json) => {
