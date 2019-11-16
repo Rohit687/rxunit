1e3d7d4ee62f4333c56b1c2e69d83e9b1cebcd8d
Ajay Kumar <devakm95@gmail.com>
2019-11-16 14:27:09 +0530

    Remove header

--- assets/js/gsscript.js
+++ assets/js/gsscript.js
@@ -23,11 +23,7 @@ const app = function () {
 	function _getPosts () {
 		_setNotice('<img class="loader" src="./assets/img/lo.jpg" alt="loading"/>');
 
-		fetch(_buildApiUrl(state.activePage, state.activeCategory),{
-		headers: new Headers({
-			'Access-Control-Allow-Origin': '*'
-		})}
-)
+		fetch(_buildApiUrl(state.activePage, state.activeCategory))
 			.then((response) => response.json())
 			.then((json) => {
 				if (json.status !== 'success') {
